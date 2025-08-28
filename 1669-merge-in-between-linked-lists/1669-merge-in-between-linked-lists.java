da1bddbeee012643347fadae01911bd39730b56a
Badavath Nagaraju <nagarajnayakbadavath@gmail.com>
2025-08-28 13:59:53 +0530

    Time: 1 ms (99.85%), Space: 46.4 MB (78.86%) - LeetHub

---
+++ 1669-merge-in-between-linked-lists/1669-merge-in-between-linked-lists.java
@@ -0,0 +1,32 @@
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
+class Solution {
+    public ListNode mergeInBetween(ListNode list1, int a, int b, ListNode list2) {
+
+        ListNode prevA=list1;
+
+        for(int i=0;i<a-1;i++){
+            prevA=prevA.next;
+        }
+        ListNode afterB=prevA;
+        for(int i=a;i<=b;i++){
+            afterB=afterB.next;
+        }
+        afterB=afterB.next;
+        prevA.next=list2;
+
+        while(list2.next!=null){
+            list2=list2.next;
+        }
+        list2.next=afterB;
+        return list1;
+    }
+}
