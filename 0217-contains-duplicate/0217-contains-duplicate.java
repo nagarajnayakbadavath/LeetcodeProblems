f7a59aded9279dc577ccc671b0424c7a251e9981
Badavath Nagaraju <nagarajnayakbadavath@gmail.com>
2025-11-18 18:27:57 +0530

    Time: 22 ms (13.71%), Space: 94.9 MB (6.31%) - LeetHub

--- 0217-contains-duplicate/0217-contains-duplicate.java
+++ 0217-contains-duplicate/0217-contains-duplicate.java
@@ -1,11 +1,31 @@
 class Solution {
     public boolean containsDuplicate(int[] nums) {
-        Arrays.sort(nums);
+        // Arrays.sort(nums);
 
-        for(int i=1;i<nums.length;i++){
-            if(nums[i-1]==nums[i]){
+        // for(int i=1;i<nums.length;i++){
+        //     if(nums[i-1]==nums[i]){
+        //         return true;
+        //     }
+        // }
+        // return false;
+
+        // After sorting the array the time complexity is o n(log n)
+
+        // optimal then that is O(n)
+        // we can use the HashSet which stores only the unique integer values
+
+        // Hashset<Integer> set=new HashSet<>();
+        // for(int i:nums){
+        //     set.add(i);
+        // }
+        // return set.size()==nums.length?true:false;
+
+        HashMap<Integer,Integer> map=new HashMap<>();
+        for(int i:nums){
+            if(map.containsKey(i)){
                 return true;
             }
+            map.put(i,map.getOrDefault(i,0)+1);
         }
         return false;
     }
