c7b7152295c5b69ea7db7307ae93a1b54b7ce016
Badavath Nagaraju <nagarajnayakbadavath@gmail.com>
2025-11-20 17:00:54 +0530

    Time: 2 ms (82.73%), Space: 47.5 MB (23.01%) - LeetHub

--- 0283-move-zeroes/0283-move-zeroes.java
+++ 0283-move-zeroes/0283-move-zeroes.java
@@ -1,15 +1,23 @@
 class Solution {
     public void moveZeroes(int[] nums) {
-        
+        int count=0;
+        for(int i=0;i<nums.length;i++){
+            if(nums[i]==0){
+                count++;
+            }
+        }
+        int temp[]=new int[nums.length];
         int index=0;
         for(int i=0;i<nums.length;i++){
             if(nums[i]!=0){
-                nums[index++]=nums[i];
+                temp[index++]=nums[i];
             }
         }
-
-        while(index<nums.length){
-            nums[index++]=0;
+        for(int i=index;i<nums.length;i++){
+            temp[i]=0;
+        }
+        for(int i=0;i<nums.length;i++){
+            nums[i]=temp[i];
         }
     }
 }
